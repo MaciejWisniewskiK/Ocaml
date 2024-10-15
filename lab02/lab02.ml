fcc3caa69d678f079dfb2402273b3e3899efa473
MaciejWisniewskiK <maciej.wisniewski.k@gmail.com>
2024-10-16 00:09:27 +0200

    comments

--- lab02/lab02.ml
+++ lab02/lab02.ml
@@ -1,14 +1,14 @@
 (* Task 1 *)
 (* examples of lists of lists*)
-[[];[]];;
-[[[]]];;
-[[]; [[]]];;
+[[];[]];; (*'a list list*)
+[[[]]];; (*'a list list list*)
+[[]; [[]]];; (* expression is correct *)
 
 (* Task 2 *)
 (* Take second element of the first list and first element of the second list *)
 let sec_fi_and_fi_sec l = match l with
     | (_ :: p :: _) :: (d :: _) :: _ -> p + d
-    | _ -> invalid_arg "za krótka lista"
+    | _ -> invalid_arg "invalid arg"
 ;;
 
 (* Task 3 *)
@@ -26,14 +26,14 @@ let len2 l =
 ;;
 
 (* generate list of natural numbers up to n *)
-let rec rosnace1 n =
+let rec increasing1 n =
     let rec pom ak n =
         if n = 0 then ak else pom (n :: ak) (n - 1)
     in
     pom [] n
 ;;
 
-let rec rosnace2 n =
+let rec increasing2 n =
     let rec pom s e =
         if s > e then [] else s :: pom (s + 1) e
     in
@@ -53,7 +53,8 @@ let rev l =
 
 (* Task 5 *)
 (* Take a list without the last element *)
-let bez_ost l =
+(* This function works in linear time and can't be written faster *)
+let no_last l =
     let rec pom ak l = match l with
         | [] -> ak
         | _ :: [] -> ak
@@ -63,15 +64,16 @@ let bez_ost l =
 ;;
 
 (* Generate a list of sufixes *)
-let rec ogony l = match l with
+let rec suf l = match l with
   | [] -> [[]]
-  | _ :: t -> l :: ogony t
+  | _ :: t -> l :: suf t
 ;;
 
 (* Generate a list of prefixes *)
-let rec poczatki l = match l with
+(* pref function can't be written in linear time or space *)
+let rec pref l = match l with
     | [] -> [[]]
-    | _ -> l :: poczatki (bez_ost l)
+    | _ -> l :: pref (no_last l)
 ;;
 
 (* Task 6 *)
@@ -88,19 +90,19 @@ let f a b l = match l with
 
 (* Task 7 *)
 (* reverse all sublists *)
-let odwroc_wszystkie l = 
+let rev_all l = 
     map rev l
 ;;
 
 (* Task 8 *)
 (* get only the even numbers *)
-let parzyste l = 
+let even l = 
     List.filter (fun x -> if x mod 2 == 0 then true else false) l
 ;;
 
 (* Task 9 *)
 (* get only the even numbers and divide by 2 *)
-let polowki l =
-    map (fun x -> x/2) (parzyste l)
+let halves l =
+    map (fun x -> x/2) (even l)
 ;;
 
